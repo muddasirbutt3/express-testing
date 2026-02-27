faf0e4d5ec834ea52f6b32c8595d66e5e09f197b
Muddasir Butt <muddasirbutt3@gmail.com>
2026-02-27 23:22:39 +0500

    nodemail

---
+++ sendMail.js
@@ -0,0 +1,60 @@
+// import { Resend } from "resend";
+
+// const resend = new Resend(process.env.RESEND_API_KEY);
+
+// async function sendEmail() {
+//   const response = await resend.emails.send({
+//     from: "Acme <onboarding@resend.dev>",
+//     to: "muddasirbutt3@gmail.com",
+//     subject: "Hello from my project",
+//     html: "<strong>Email works!</strong>",
+//   });
+
+//   return response;
+// }
+// export default sendEmail;
+// import {MailtrapTransport} from 'mailtrap'
+
+// const TOKEN = process.env.MAIL_TRAP_API_KEY;
+
+// const transport = Nodemailer.createTransport(
+//   MailtrapTransport({
+//     token: TOKEN,
+//   }),
+// );
+
+// const sender = {
+//   address: "hello@demomailtrap.co",
+//   name: "Mailtrap Test",
+// };
+// const recipients = ["muddasirbutt3@gmail.com"];
+
+// transport
+//   .sendMail({
+//     from: sender,
+//     to: recipients,
+//     subject: "You are awesome!",
+//     text: "Congrats for sending test email with Mailtrap!",
+//     category: "Integration Test",
+//   })
+//   .then(console.log, console.error);
+import nodemailer from "nodemailer";
+
+const transporter = nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+    user: process.env.GMAIL,
+    pass: process.env.APP_PASSWORD,
+  },
+});
+
+async function sendMail() {
+  return await transporter.sendMail({
+    from: "My App <muddasirbutt3@gmail.com>",
+    to: "muddasirbutt4fun@gmail.com",
+    subject: "Welcome",
+    text: "Thanks for signing up!",
+  });
+}
+
+export default sendMail;
--- server.js
+++ server.js
@@ -1,12 +1,22 @@
-import express from 'express'
-const app = express()
+import "dotenv/config";
+import express from "express";
+import connectDB from "./connectDB.js";
+import sendEmail from "./sendMail.js";
+const app = express();
 
+connectDB();
 
+app.get("/", (req, res) => {
+  res.status(200).json({ success: true, message: "Everything working fine." });
+});
 
+app.get("/send-email", async (req, res) => {
+  const result = await sendEmail();
+  console.log(result);
 
+  res.status(200).json({ success: true, message: "Email is send." });
+});
 
-
-app.listen(3000,()=>{
-    console.log('runn ');
-    
-})
+app.listen(3000, () => {
+  console.log("server started successfully");
+});
